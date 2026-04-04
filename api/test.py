0df3db123c47a052ee6a02d1a5770e564463f908
Nausheen Suraj <nausheensuraj@gmail.com>
2026-04-04 17:43:15 +0800

    Remove test.py file

--- api/test.py
+++
@@ -1,8 +0,0 @@
-def handler(request):
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'application/json'
-        },
-        'body': '{"status": "ok", "message": "API is working"}'
-    }
